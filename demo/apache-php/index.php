12f3d8fd79272ed4187834a4795efbafc24a7097
Jonathan Neal <jonathantneal@hotmail.com>
2013-09-11 12:54:30 -0700

    Demo
    Improved styling, remove log, added description.

--- demo/apache-php/index.php
+++ demo/apache-php/index.php
@@ -4,19 +4,19 @@
 <meta name="viewport" content="width=device-width,initial-scale=1">
 <style>
 html { font: 300 100%/1.5 "Helvetica Neue Light", "Helvetica Neue", Helvetica, Arial, "Lucida Grande", sans-serif; }
-body { margin: 0 auto; max-width: 44em; padding: 0 .5em; }
+body { margin: 0 auto; max-width: 40em; padding: 0 .5em; }
 img { max-width: 100%; }
 </style>
 <?php if (!isset($_SERVER['HTTP_CH'])) print(
 	'<script>'.
-	'console.log(document.cookie="'.
+	'document.cookie="'.
 		'CH='.
 			'dh="+screen.height+",'.
 			'dpr="+(window.devicePixelRatio||1)+",'.
 			'dw="+screen.width+"'.
 		';'.
 		'expires="+new Date(+new Date+31536000000).toGMTString()+";'.
-		'path=/")'.
+		'path=/"'.
 	'</script>'
 ); ?>
 
@@ -24,7 +24,11 @@ img { max-width: 100%; }
 
 <p><img src="image/kitten.jpg" alt="Kitten" id="image"></p>
 
-<p>We sent you a <span id="resolution"></span> image.</p>
+<p>
+	This device received a <span id="resolution"></span> image.
+	Standard devices receive a 640x360 image (25KB).
+	HiDPI devices receive a 1280x720 image (49KB).
+</p>
 
 <script>
 window.onload = function () {
